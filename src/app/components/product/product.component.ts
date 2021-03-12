11eb5ca12ecc01c6e89d5e63662c1de8dc823d9f
busraglk <busragelik@gmail.com>
2021-03-12 04:44:31 +0300

    commit 2

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
+
 
 @Component({
   selector: 'app-product',
@@ -7,12 +9,21 @@ import { Product } from 'src/app/models/product';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
-
-  products:Product[] = [];
-  constructor() { }
+    products: Product[] = [];
+    dataLoaded = false;
+   
+ 
+  constructor(private productService:ProductService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+   this.productService.getProducts().subscribe(response =>{
+     this.products = response.data
+     this.dataLoaded = true;
+   })
   }
 
 }
