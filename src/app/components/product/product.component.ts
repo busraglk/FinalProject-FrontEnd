cb2ea3fd7da4860e58249d0fee24ff2606bba72f
busraglk <busragelik@gmail.com>
2021-03-15 03:52:51 +0300

    commit 2

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -11,11 +12,17 @@ export class ProductComponent implements OnInit {
     products: Product[] = [];
     dataLoaded = false;
    
- 
-  constructor(private productService:ProductService) { }
+  constructor(private productService:ProductService, 
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.getProducts();
+    this.activatedRoute.params.subscribe(params => {
+      if(params["categoryId"]){
+        this.getProductsByCategory(params["categoryId"])
+      }else{
+        this.getProducts()
+      }
+    })
   }
 
   getProducts() {
@@ -24,5 +31,12 @@ export class ProductComponent implements OnInit {
      this.dataLoaded = true;
    })
   }
+
+  getProductsByCategory(categoryId: number) {
+    this.productService.getProductsByCategory(categoryId).subscribe(response =>{
+      this.products = response.data
+      this.dataLoaded = true;
+    })
+   }
 
 }
